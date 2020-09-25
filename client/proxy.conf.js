ad3296e0b1230a9c140c3bdff64c604318b6e7d9
Aleksei Borisov <borisov.aleksei.1903@gmail.com>
2020-09-25 23:37:20 +0300

    proxy config, login and register components

---
+++ client/proxy.conf.js
@@ -0,0 +1,11 @@
+module.exports = [
+  {
+    context: [
+      '/api/*'
+    ],
+    target: 'http://localhost:3000',
+    secure: false,
+    logLevel: 'debug',
+    changeOrigin: true
+  }
+]
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,20 +1,29 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatSliderModule } from '@angular/material/slider';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatSliderModule} from '@angular/material/slider';
+import {LoginPageComponent} from './login-page/login-page.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AuthLayoutComponent} from './shared/layouts/auth-layout/auth-layout.component';
+import {RegisterPageComponent} from './register-page/register-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPageComponent,
+    AuthLayoutComponent,
+    RegisterPageComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MatSliderModule
+    MatSliderModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
